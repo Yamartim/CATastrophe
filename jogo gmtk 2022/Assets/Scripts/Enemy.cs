f9ed924bc8c3caaf85322c246a8124ba2e748200
FLMBorges <felipe.lepore@hotmail.com>
2022-07-17 13:43:07 -0300

    Bg e tiro consertados

--- jogo gmtk 2022/Assets/Scripts/Enemy.cs
+++ jogo gmtk 2022/Assets/Scripts/Enemy.cs
@@ -61,6 +61,7 @@ public class Enemy : MonoBehaviour
         if (collision.gameObject.tag == "Bullet")
         {
             Destroy(collision.gameObject);
+            TakeDamage(20);
         }
     }
 
